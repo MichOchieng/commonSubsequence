7e2350139ebbba9513f0bf2f99131590eb4d6a69
Mich Ochieng <michochieng@gmail.com>
2022-02-11 01:14:08 -0800

    First commit

---
+++ subsequence.py
@@ -0,0 +1,10 @@
+class subsequence:
+    def __init__(self) -> None:
+        pass
+
+    def run(self):
+        print("Test")
+
+if __name__ == "__main__":
+    program = subsequence()
+    program.run()
