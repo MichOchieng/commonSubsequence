bfcf6d05c3063413854fcd9145c99d2ce377a747
Mich Ochieng <michochieng@gmail.com>
2022-02-12 15:22:34 -0800

    Added way to find input files

--- subsequence.py
+++ subsequence.py
@@ -1,9 +1,25 @@
+import re
+from os import walk
+from pathlib import Path
+
 class subsequence:
     def __init__(self) -> None:
-        pass
+        self.getFiles()
+
+    FILE_LIST = []
+
+    def getFiles(self):
+        # Scan the directory for text input files with the naming convention input_*.txt (where * is some number)
+        currentPath = Path(__file__).parent.resolve()
+        # Loop through files in directory and find input files
+        for file in next(walk(currentPath), (None, None, []))[2]:
+            if re.search("^input_.*.txt",file):
+                self.FILE_LIST.append(file)
+                print(file)
+        
 
     def run(self):
-        print("Test")
+        pass
 
 if __name__ == "__main__":
     program = subsequence()
